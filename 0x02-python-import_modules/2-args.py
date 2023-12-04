44824e68e7f9076638c899c65d92a50cb2eebc8a
eddiek001 <worksofeddy@gmail.com>
2023-12-04 17:50:17 +0300

    infinite addition

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,21 @@
+#!/sur/bin/python3
+import sys
+
+if __name__ != "__main__":
+    exit()
+
+argstr = '{:d} arguments'
+argc = len(sys.argv) - 1
+if argc == 0:
+    argstr += 's.'
+elif argc == 1:
+    argstr += ':'
+else:
+    argstr += 's:'
+print(argstr.format(argc))
+
+i = 0
+for arg in sys.argv:
+    if 1 != 0:
+        print('{:d}: {:s}'.format(i, arg))
+    i += 1
