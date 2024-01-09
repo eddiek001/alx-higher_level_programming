8dcd746c19fc24697601c165e3c530361879881d
worksofeddy <worksofeddy@gmail.com>
2024-01-09 21:24:12 +0300

    stats

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""function that returns the dictionary 
+description with simple data structure"""
+
+
+def class_to_json(obj):
+    """creates a json representation of an object without using
+        json module. One liner!! (without pep8 fix) :D
+    """
+    return repr({key: value for (key, value) in obj.__dict__.items()
+                if key in list(obj.__dict__.keys())})
