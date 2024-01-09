03abfb8cba575304eedd2f5dd6d058c69a632efb
worksofeddy <worksofeddy@gmail.com>
2024-01-09 22:13:32 +0300

    student 11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -13,22 +13,16 @@ class Student:
     def to_json(self, attrs=None):
         """retrieves dict rep of Student in json form
         """
-        is_all_strs = True
-        if isinstance(attrs, list):
-            for ele in attrs:
-                if not isinstance(ele, str):
-                    is_all_strs = False
-        else:
-            is_all_strs = False
+        if attrs is None:
+            return self.__dict__
 
-        return {key: value for (key, value) in self.__dict__.items()
-                if key in list(self.__dict__.keys()) and
-                (not is_all_strs or key in attrs)}
+        return {key: value for key, value in
+                self.__dict__.items() if key in attrs}
 
     def reload_from_json(self, json):
         """reloads an object from a json dictionary
             -> assumes json to always be a dictionary
         """
-        self.first_name = json['first_name']
-        self.last_name = json['last_name']
-        self.age = json['age']
+        for key in ('first_name', 'last_name', 'age'):
+            if key in json:
+                setattr(self, key, json[key])
