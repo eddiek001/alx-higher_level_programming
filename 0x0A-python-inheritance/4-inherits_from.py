5f4eceb9f1b8df5bcd0c8681c0eaf7d19ba11d19
worksofeddy <worksofeddy@gmail.com>
2024-01-10 20:08:03 +0300

    inherits

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -10,4 +10,4 @@ def inherits_from(obj, a_class):
     if not isinstance(a_class, type):
         raise TypeError("a_class must be of type 'type'")
 
-    return isinstance(obj, a_class) and type(obj) != a_class
+    return issubclass(type(obj), a_class) and type(obj) != a_class
